ecdd0ab62c685cc2b4590ad98128b9897b4e5e24
Ben Kolera <ben.kolera@gmail.com>
2014-10-06 16:31:51 +1000

    And just some general fiddling before bananas start flying.

--- gorillas.elm
+++ gorillas.elm
@@ -1,50 +1,86 @@
 import Keyboard
+import Maybe (maybe)
 import Window
 
 data Direction = Left | Right
 type Gorilla = 
-  { x        : Float 
-  , dir      : Direction 
-  , throwing : Bool 
+  { x         : Float 
+  , direction : Direction 
+  , throwing  : Bool 
   }
 
-type Model = { gorilla1 : Gorilla , banana : Maybe Banana }
+type Model = 
+  { gorilla1 : Gorilla 
+  , gorilla2 : Gorilla 
+  , banana   : Maybe Banana 
+  }
+
 type Banana = 
-  { x  : Float 
-  , y  : Float
-  , dx : Float 
-  , dy : Float
+  { x         : Float 
+  , y         : Float
+  , dx        : Float 
+  , dy        : Float
+  , frame     : Int
+  , direction : Direction
   }
 
-gorilla = { x = 0 , dir = Left , throwing = False }
-model   = { gorilla1 = gorilla , banana = Nothing }
+gorilla1 : Gorilla           
+gorilla1 = { x = -800 , direction = Left , throwing = False }
+gorilla2 : Gorilla           
+gorilla2 = { x = 800  , direction = Right , throwing = False }
+model : Model
+model    = { gorilla1 = gorilla1, gorilla2 = gorilla2 , banana = Nothing }
 
 stepGorilla : Bool -> Gorilla -> Gorilla 
 stepGorilla isSpace g = { g | throwing <- isSpace}
 
 step : Bool -> Model -> Model
-step isSpace game = { game | gorilla1 <- stepGorilla isSpace game.gorilla1 }
+step isSpace game = 
+  { game | gorilla1 <- stepGorilla isSpace game.gorilla1 }
+
+directionString : Direction -> String
+directionString d = case d of 
+  Left  -> "left"
+  Right -> "right"
+
+imagePath : String -> Direction -> String -> String
+imagePath name dir frame = 
+  join "/" ["images",name,directionString dir,frame ++ ".png"]
 
 gorillaForm : Float -> Gorilla -> Form
 gorillaForm groundY g =   
-  let frame        = if g.throwing then "1" else "0"
-      src          = "images/gorilla_" ++ frame ++ ".png"
-      gorillaImage = image 35 35 src
+  let frame        = if g.throwing then "throwing" else "base"
+      src          = imagePath "gorilla" g.direction frame
+      gorillaImage = image 60 60 src
   in gorillaImage |> toForm |> move (g.x,groundY)
+
+skyForm : Float -> Float -> Form 
+skyForm w h = rect w h |> filled (rgb 174 238 238)
+
+bananaForm : Banana -> Form
+bananaForm b =   
+  let src = imagePath "banana" b.direction (show b.frame)
+      img = image 20 20 src
+  in img |> toForm |> move (b.x,b.y)
+
+groundForm : Float -> Float -> Form
+groundForm w h =
+  rect w 50 |> filled (rgb 74 167 43) |> move (0, 24 - h/2)
+
+maybeBananaForm : Maybe Banana -> [Form]
+maybeBananaForm mb = maybe [] (\ x -> [bananaForm x]) mb
 
 display : (Int, Int) -> Model -> Element
 display (w',h') m =
   let (w,h)      = (toFloat w', toFloat h')
-      src        = "images/gorilla_0.png"
       groundY    = 62 - h/2
   in
-      collage w' h'
-          [ rect w h |> filled (rgb 174 238 238)
-          , rect w 50
-              |> filled (rgb 74 167 43)
-              |> move (0, 24 - h/2)
-          , gorillaForm groundY m.gorilla1
-          ]
+    collage w' h' 
+      (  skyForm w h
+      :: groundForm w h
+      :: gorillaForm groundY m.gorilla1
+      :: gorillaForm groundY m.gorilla2
+      :: maybeBananaForm m.banana )
       
 main : Signal Element
 main = lift2 display Window.dimensions (foldp step model input)
